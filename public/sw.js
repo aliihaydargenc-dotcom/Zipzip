229fb8475c72c56885848be0810f9f601f3840aa
aliihaydargenc-dotcom <aliihaydargenc@gmail.com>
2026-09-22 17:32:39 +0300

    feat: rebuild Zipzip core gameplay loop

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zipzip-v4-audio-fragile-1';
+const CACHE = 'zipzip-v5-core-gameplay-1';
 const scope = self.registration.scope;
 const shell = [scope, new URL('manifest.webmanifest', scope).href, new URL('zipzip-icon.svg', scope).href];
 
